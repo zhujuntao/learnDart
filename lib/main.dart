4e2b30a096334795d3de8ed876f52ef51a9ccf3c
zhujuntao <juntao.zhu@pactera.com>
2020-06-29 14:58:00 +0800

    flutter入口自定义组件

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,24 @@ void main() {
   runApp(Myapp());
 }
 
+//无状态组件 StatelessWidget
+//MaterialApp 封装了实现Material desjgn 所需的一些Widget 一般作为顶层的Widget
+
 class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //主页
       home: Scaffold(
+        //显示在页面顶部
         appBar: AppBar(
           title: Text('Flutter Demo'),
           centerTitle: true,
         ),
+        //当前界面显示的主要内容
         body: HomeContent(),
       ),
-      theme: ThemeData(primarySwatch: Colors.pink),//设置主题颜色
+      theme: ThemeData(primarySwatch: Colors.pink), //设置主题颜色
     );
   }
 }
@@ -26,7 +32,7 @@ class HomeContent extends StatelessWidget {
     return Center(
       child: Text(
         '你好 flutter 1111',
-        textDirection: TextDirection.ltr,
+        textDirection: TextDirection.ltr, //文本显示方式
         style: TextStyle(
           fontSize: 40.0,
           //color: Colors.blueAccent,
