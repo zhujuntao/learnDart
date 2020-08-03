c2d88f4c0d9ddb0e0f8ff8d51f9ee101e7a6e258
zhujuntao <juntao.zhu@pactera.com>
2020-08-03 17:30:41 +0800

    wrap使用

--- lib/flutter/layout/wrap_test.dart
+++ lib/flutter/layout/wrap_test.dart
@@ -19,6 +19,31 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/*
+Key key,
+this.direction = Axis.horizontal,//设置水平局部还是垂直布局
+this.alignment = WrapAlignment.start,//设置元素的其实位置，
+
+this.spacing = 0.0,//在direction: Axis.horizontal的时候指左右两个Widget的间距,在direction: Axis.vertical的时候指上下两个widget的间距
+this.runAlignment = WrapAlignment.start,//设置widget与widget在水平或者垂直轴上的间距
+WrapAlignment.start//每一行（列）子Widget在纵（横）轴上的排列，全部子Widget从顶部开始展示
+WrapAlignment.end//每一行（列）子Widget在纵（横）轴上的排列，全部子Widget挨着底部展示
+WrapAlignment.center//每一行（列）子Widget在纵（横）轴上的排列，全部子widget在中间展示
+WrapAlignment.spaceBetween//每一行（列）子Widget在纵（横）轴上的排列，两两子widget之间间距相等，最上最下子widget挨着边展示
+WrapAlignment.spaceAround//每一行（列）子Widget在纵（横）轴上的排列，两两子widget之间间距相等，最上最下子widget离边的距离为两两子widget之间距离的一半
+WrapAlignment.spaceAround//每一行（列）子Widget在纵（横）轴上的排列，两两子widget之间间距相等，最上最下子widget离边的距离为两两子widget之间距离相等
+this.runSpacing = 0.0,//在direction: Axis.horizontal的时候指上下两个Widget的间距,在direction: Axis.vertical的时候指左右两个widget的间距
+this.crossAxisAlignment = WrapCrossAlignment.start,//水平排列时控制全部子widgets的上部对齐，垂直排列时控制全部子widgets的左边对齐
+WrapCrossAlignment.end//水平排列时控制全部子widgets的下部对齐，垂直排列时控制全部子widgets的又边对齐
+WrapCrossAlignment.center//设置每一行的子Widgets剧中对齐
+this.textDirection,//设置每一行（列）的展示
+textDirection: TextDirection.ltr//设置每一行（列）的子Widgets从左到右（从上到下）正常显示，正序排列
+textDirection: TextDirection.rtl////设置每一行（列）的子Widgets倒序显示
+this.verticalDirection = VerticalDirection.down,//设置行列widgets的展示，正常显示
+VerticalDirection.up//倒序展示，比如，在direction: Axis.horizontal时有1、2、3行widgets，设置为up后，展示为3、2、1
+List<Widget> children = const <Widget>[],//一组子widgets
+*/
+
 class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
